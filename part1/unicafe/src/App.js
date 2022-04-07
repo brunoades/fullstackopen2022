2c91b09549c961d28536a04fb91626842d950df9
Bruno Alves <brunoades@gmail.com>
2022-04-07 16:24:14 -0300

    1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -24,12 +24,20 @@ const Statistics = ({good, neutral, bad, all, average, positive}) => {
   }
   return (
     <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={all} />
+      <StatisticLine text='average' value={average} />
+      <StatisticLine text='positive' value={positive} />
+    </>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <>
+      <p>{props.text} {props.value}</p>
     </>
   )
 }
