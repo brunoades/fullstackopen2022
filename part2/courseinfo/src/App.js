7ac9b73ee39a88d0d07fae556ea9ba86e5cd846c
Bruno Alves <brunoades@gmail.com>
2022-04-13 13:25:06 -0300

    2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,44 +1,4 @@
-const Header = ({header}) => {
-  return (
-    <>
-      <h2>{header}</h2>
-    </>
-  )
-}
-const Content = ({parts}) => {
-  return (
-    <>
-      <Part  parts={parts}/>
-    </>
-  )
-}
-const Part = ({parts}) => {
-  return (
-    <>
-      {parts.map(p => <p key={p.id}>{p.name} {p.exercises}</p>)}
-    </>
-  )
-}
-const Total = ({total}) => {
-  return (
-    <>
-      <p>total of {total} exercises</p>
-    </>
-  )
-}
-const Course = ({header, parts}) => {
-  const exercises = []
-  parts.map(e => exercises.push(e.exercises))
-  const total = exercises.reduce((s, p) => s+= p)
-  return (
-    <>
-      <Header header={header}/>
-      <Content parts={parts}/>
-      <Total total={total}/>
-    </>
-  )
-}
-
+import Course from './modules/Course'
 const App = () => {
   const courses = [
     {
