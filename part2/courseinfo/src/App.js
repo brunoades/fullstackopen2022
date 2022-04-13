d448f9ac0178e49e041311feb6da902f92fa452f
Bruno Alves <brunoades@gmail.com>
2022-04-13 09:48:29 -0300

    2.1: Course information step6

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,49 +1,75 @@
-import { useState } from "react"
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <>
-        <p>
-        the app is used by pressing the buttons
-        </p>
-      </>
-    )
-  }
+const Header = ({header}) => {
   return (
     <>
-      <p>
-      button press history: {props.allClicks.join(' ')}
-      </p>
+      <h1>{header}</h1>
     </>
   )
 }
-const Button = ({ handleClick, text}) => {
+
+const Content = ({content, part}) => {
   return (
     <>
-      <button onClick={handleClick}>{text}</button>
+      <p>{content} <Part part={part}/></p>
     </>
   )
 }
+
+const Part = ({part}) => {
+  return (
+    <>
+      <span> {part}</span>
+    </>
+  )
+}
+
+const Course = ({header, content, part}) => {
+  return (
+    <>
+      <Header header={header} />
+      <Content content={content} part={part} />
+    </>
+  )
+}
+
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  } 
   return (
     <>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <Course
+        header={course.name}
+        content={course.parts[0].name}
+        part={course.parts[0].exercises}
+      />
+      <Course
+        content={course.parts[1].name}
+        part={course.parts[1].exercises}
+      />
+      <Course
+        content={course.parts[2].name}
+        part={course.parts[2].exercises}
+      />
     </>
   );
 }
+
 export default App;
