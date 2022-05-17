2208c65f8e3d89cc7333e172038617c9b3a42c4b
Bruno Alves <brunoades@gmail.com>
2022-05-17 20:43:54 -0300

    2.6: The Phonebook Step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,46 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persosns, serPersons] = useState([
-    { name: 'Arto Hellas'}
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      id: 1
+    }
   ])
+
+  const [newPerson, setNewPerson] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newPerson,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(personObject))
+    setNewPerson('')
+  }
+  const handleNameChange = (event) => {
+    setNewPerson(event.target.value)
+  }
+
   return (
     <>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input
+            value={newPerson}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type='submit' >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>
+        {persons.map(person =>
+          <p key={person.name}>{person.name}</p>)}
+      </div>
     </>
   )
 }
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.render(
-  <App notes={notes} />,
+  <App />,
   document.getElementById('root')
 )
